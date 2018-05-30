6ea61e9491b8f48952d2bf76860fd5d9af1f35f0
Kunal Thakur <KunalThakur@kunals-mbp-2.coldspring.marian.edu>
2018-05-30 15:34:13 -0400

    update new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import './App.css'
 import { auth } from './base'
@@ -7,10 +8,14 @@ import SignIn from './SignIn'
 
 class App extends Component {
   state = {
-    uid: null
+    uid: null,
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    const uid = localStorage.getItem('uid')
+    if (uid) {
+      this.setState({ uid })
+    }
     auth.onAuthStateChanged(user => {
       if (user) {
         this.handleAuth(user)
@@ -22,6 +27,7 @@ class App extends Component {
 
   handleAuth = (user) => {
     this.setState({ uid: user.uid })
+    localStorage.setItem('uid', user.uid)
   }
 
   signedIn = () => {
@@ -30,17 +36,42 @@ class App extends Component {
 
   signOut = () => {
     this.setState({ uid: null })
+    localStorage.removeItem('uid')
     auth.signOut()
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.signedIn()
-            ? <Main signOut={this.signOut} />
-            : <SignIn handleAuth={this.handleAuth} />
-        }
+        <Switch>
+          <Route
+            path="/sign-in"
+            render={navProps => (
+              this.signedIn()
+                ? <Redirect to="/notes" />
+                : <SignIn {...navProps} />
+            )}
+          />
+          <Route
+            path="/notes"
+            render={navProps => (
+              this.signedIn()
+               ? <Main
+                   signOut={this.signOut}
+                   uid={this.state.uid}
+                   {...navProps}
+                 />
+               : <Redirect to="/sign-in" />
+            )}
+          />
+          <Route
+            render={() => (
+              this.signedIn()
+                ? <Redirect to="/notes" />
+                : <Redirect to="/sign-in" />
+            )}
+          />
+        </Switch>
       </div>
     )
   }
